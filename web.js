0aefe127826a0d3e4e2129ef3d51b1463f3df2a8
palterity <pferlizi@gmail.com>
2013-07-11 11:12:30 +0100

    Corrected error web.js 1

--- web.js
+++ web.js
@@ -2,7 +2,7 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-var text = fs.readFileSync(index.html);
+var text = fs.readFileSync('index.html', 'utf-8');
 
 var buffer = new Buffer(text, "utf-8");
 
